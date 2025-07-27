75cded3d2d75a55f1e96f6f843a26752d0af414f
Masud Rana <71147447+mr-masudrana@users.noreply.github.com>
2025-07-27 11:10:39 +0600

    Create Post.js

---
+++ models/Post.js
@@ -0,0 +1,26 @@
+// facebook-clone-backend/models/Post.js
+
+const mongoose = require("mongoose");
+
+const PostSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Post", PostSchema);
