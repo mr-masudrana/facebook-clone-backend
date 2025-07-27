62101d164de65a84e59dfc2da8729dfc0ea5b41d
Masud Rana <71147447+mr-masudrana@users.noreply.github.com>
2025-07-27 11:14:29 +0600

    Create server.js

---
+++ server.js
@@ -0,0 +1,26 @@
+// facebook-clone-backend/server.js
+
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const authRoutes = require("./routes/auth");
+const postRoutes = require("./routes/posts");
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.use("/api/auth", authRoutes);
+app.use("/api/posts", postRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
